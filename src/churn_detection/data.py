dbabf025586594ee20cbea545c8b0f06e9f85b79
joelabl <82639814+joelabl@users.noreply.github.com>
2024-11-23 16:33:14 +0100

    Update and refactor data collection process in data.py and 01-fetch_batch_data_from_kaggle.ipynb

--- src/churn_detection/data.py
+++ src/churn_detection/data.py
@@ -4,66 +4,76 @@ from a Kaggle dataset for further analysis.
 
 The script provides utility functions to:
 - Fetch data from Kaggle and extract it from a zip file.
-- Save the data in Feather format for optimized storage and retrieval.
+- Save the data in the specified format (Feather or CSV) for optimized storage and retrieval.
 """
 
 import subprocess
 import zipfile
-from pathlib import Path
+import os
 from typing import NoReturn
 
 import pandas as pd
 
-from churn_detection.paths import EXTERNAL_DATA_DIR
 
-CURRENT_DIR = Path().cwd()
-ZIP_FILE = Path("telco-customer-churn.zip")
-CSV_FILE = Path("WA_Fn-UseC_-Telco-Customer-Churn.csv")
-
-
-def fetch_batch_data(target: str) -> pd.DataFrame:
+def fetch_batch_data(
+    target: str,
+    cwd_path: str | os.PathLike,
+    zip_file: str | os.PathLike,
+    raw_data: str | os.PathLike,
+) -> pd.DataFrame:
     """
     Downloads the specified Kaggle dataset, extracts the CSV file from the zip archive,
     and loads it into a pandas DataFrame.
 
     Args:
-        target (str): The Kaggle dataset to download, e.g. 'blastchar/telco-customer-churn'.
+        target (str): The Kaggle dataset identifier to fetch, e.g. 'blastchar/telco-customer-churn'.
+        cwd_path (str | os.PathLike): The path to the CWD where files will be extracted.
+        zip_file (str | os.PathLike): The path to the zip file to be downloaded.
+        raw_data (str | os.PathLike): The path to the extracted CSV file.
 
     Returns:
         pd.DataFrame: A DataFrame containing the extracted customer churn data.
     """
     subprocess.run(["kaggle", "datasets", "download", "-d", target], check=True)
-    with zipfile.ZipFile(ZIP_FILE, "r") as zip_ref:
-        zip_ref.extractall(CURRENT_DIR)
-    data = pd.read_csv(CSV_FILE)
+    with zipfile.ZipFile(zip_file, "r") as zip_ref:
+        zip_ref.extractall(cwd_path)
+    data = pd.read_csv(raw_data)
     return data
 
 
-def save_batch_data(df: pd.DataFrame, file_format: str = "feather") -> NoReturn:
+def save_batch_data(
+    df: pd.DataFrame,
+    target_path: str | os.PathLike,
+    zip_file: str | os.PathLike,
+    raw_data: str | os.PathLike,
+    file_format: str = "feather",
+) -> NoReturn:
     """
     Saves the given DataFrame to the specified file format (Feather or CSV) and deletes
-    temporary files.
+    temporary files used during the data download and extraction process.
 
     Args:
         df (pd.DataFrame): The customer churn DataFrame to save.
-        file_format (str): The format in which to save the DataFrame. Supported formats
-        are 'csv' or 'feather'.
-                           Defaults to 'feather'.
+        target_path (str | os.PathLike): The path where the DataFrame should be saved.
+        zip_file (str | os.PathLike): The path to the zip file that was downloaded.
+        raw_data (str | os.PathLike): The path to the raw CSV file that was extracted.
+        file_format (str): The format in which to save the DataFrame. Supported formats 
+                           are 'csv' or 'feather'. Defaults to 'feather'.
 
     Raises:
         ValueError: If the provided file format is not 'csv' or 'feather'.
 
     Side Effects:
-        - Saves the DataFrame in the specified format in the external data directory.
-        - Deletes the downloaded zip and CSV files from the current directory.
+        - Saves the DataFrame in the specified format in the target directory.
+        - Deletes the downloaded zip and CSV files from the specified locations.
     """
     if file_format in ("csv", "feather"):
         if file_format == "feather":
-            df.to_feather(EXTERNAL_DATA_DIR / "customer_churn.feather")
+            df.to_feather(target_path / "customer_churn.feather")
         else:
-            df.to_csv(EXTERNAL_DATA_DIR / "customer_churn.csv", index=None)
-        subprocess.run(["cmd", "/c", "del", str(ZIP_FILE)], check=True)
-        subprocess.run(["cmd", "/c", "del", str(CSV_FILE)], check=True)
+            df.to_csv(target_path / "customer_churn.csv", index=None)
+        subprocess.run(["cmd", "/c", "del", str(zip_file)], check=True)
+        subprocess.run(["cmd", "/c", "del", str(raw_data)], check=True)
     else:
         raise ValueError(
             f"Invalid file format '{file_format}'. Supported formats are 'csv' or 'feather'."
