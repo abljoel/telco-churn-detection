0309157b67ca158256a43e417bc1e45f2354f803
joelabl <82639814+joelabl@users.noreply.github.com>
2025-01-11 22:54:08 +0100

    Update models/__init__.py to explicitly export some useful symbols

--- src/churn_detection/models/__init__.py
+++ src/churn_detection/models/__init__.py
@@ -1,3 +1,19 @@
 """Init file for churn_detection.models"""
 
+from .base_model import BaseFeatureEngineer, BaseModel, ColumnPreprocessorFeatures
+from .experiment import ExperimentManager
+from .ml_pipeline import create_pipeline, MLPipeline
+from .linear_classifier import LinearModel, SklearnModel
+
 version = "0.1"
+
+__all__ = [
+    "BaseFeatureEngineer",
+    "BaseModel",
+    "ColumnPreprocessorFeatures",
+    "ExperimentManager",
+    "create_pipeline",
+    "MLPipeline",
+    "LinearModel",
+    "SklearnModel",
+]
