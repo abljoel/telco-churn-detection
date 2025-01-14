62c08ac103ef6b0de2504d1cf1a58d6c1ccb2d80
joelabl <82639814+joelabl@users.noreply.github.com>
2025-01-14 11:49:16 +0100

    Update __init__.py in src/churn_detection to expose the most important and commonly used functions and classes of the package

--- src/churn_detection/__init__.py
+++ src/churn_detection/__init__.py
@@ -1,16 +1,90 @@
 """Init file for churn_detection"""
 
-import sys
+from .config import load_config
+from .data import load_data, fetch_batch_data, save_batch_data
+from .preprocessing import preprocess_data, clean_data, split_data
+from .evaluation import (
+    validate_model_with_cv,
+    display_roc_auc_score,
+    display_pr_auc_score,
+    display_clf_report,
+    explore_thresholds,
+)
+from .features import (
+    FeatureConcatenator,
+    RareCategoryEncoder,
+    InteractionStrengthExtractor,
+    Transformation,
+    ColumnPreprocessor,
+)
+from .utils import (
+    load_datasets,
+    get_duplicates,
+    get_dataset_info,
+    display_dataset_info,
+    get_distribution_info,
+    get_feature_names,
+)
+from .visualization import (
+    plot_pie,
+    plot_correlation_info,
+    plot_bivariate_cat,
+    plot_confusion_table,
+    churn_performance_report,
+)
+from .models import (
+    LinearModel,
+    SklearnModel,
+    ExperimentManager,
+    create_pipeline,
+    MLPipeline,
+)
+from .models.base_model import (
+    BaseModel,
+)  # Direct import since it's used by multiple modules
 
-if sys.version_info[:2] >= (3, 11):
-    from importlib.metadata import PackageNotFoundError, version
-else:
-    from importlib_metadata import PackageNotFoundError, version
+__version__ = "0.1.0"
 
-try:
-    DIST_NAME = __name__
-    __version__ = version(DIST_NAME)
-except PackageNotFoundError:
-    __version__ = "unknown"
-finally:
-    del version, PackageNotFoundError
+__all__ = [
+    # Data Loading & Config
+    "load_config",
+    "load_data",
+    "fetch_batch_data",
+    "save_batch_data",
+    # Preprocessing
+    "preprocess_data",
+    "clean_data",
+    "split_data",
+    # Evaluation
+    "validate_model_with_cv",
+    "display_roc_auc_score",
+    "display_pr_auc_score",
+    "display_clf_report",
+    "explore_thresholds",
+    # Feature Engineering
+    "FeatureConcatenator",
+    "RareCategoryEncoder",
+    "InteractionStrengthExtractor",
+    "Transformation",
+    "ColumnPreprocessor",
+    # Utilities
+    "load_datasets",
+    "get_duplicates",
+    "get_dataset_info",
+    "display_dataset_info",
+    "get_distribution_info",
+    "get_feature_names",
+    # Visualization
+    "plot_pie",
+    "plot_correlation_info",
+    "plot_bivariate_cat",
+    "plot_confusion_table",
+    "churn_performance_report",
+    # Models
+    "BaseModel",
+    "LinearModel",
+    "SklearnModel",
+    "ExperimentManager",
+    "create_pipeline",
+    "MLPipeline",
+]
